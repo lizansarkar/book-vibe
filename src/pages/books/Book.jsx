4b21786aa9a758d62026b3ffc520aa9d6565f7de
lizan sarkar <lizansarkar16@gmail.com>
2025-10-06 19:08:16 +0600

    react icons add

--- src/pages/books/Book.jsx
+++ src/pages/books/Book.jsx
@@ -1,3 +1,5 @@
+import { FaStar } from "react-icons/fa";
+
 const Book = ({ book }) => {
   console.log(book);
 
@@ -8,7 +10,7 @@ const Book = ({ book }) => {
           key={book.bookId}
           className="card bg-base-100 shadow-xl transition-all duration-300 hover:shadow-2xl hover:scale-[1.02]"
         >
-            
+
           {/* Book Image Section */}
           <figure className="px-5 pt-5 pb-2 bg-[#eeeeee] rounded-t-xl">
             {/* ছবির সাইজ ঠিক রাখার জন্য w-full h-48 object-contain ক্লাস ব্যবহার করা হয়েছে */}
@@ -49,14 +51,7 @@ const Book = ({ book }) => {
               <p className="text-xs text-gray-400">{book.category}</p>
               <div className="flex items-center text-yellow-500 text-sm">
                 {book.rating.toFixed(2)}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 ml-0.5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.817 2.045a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.817-2.045a1 1 0 00-1.175 0l-2.817 2.045c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.381-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                </svg>
+                <FaStar className=""></FaStar>
               </div>
             </div>
           </div>
